7cc62c2223f825669976f569c34d6c4c21d61317
Lokendra B. Jargha <jarghalokendra@gmail.com>
2024-05-17 12:26:54 +0545

    login form

--- mansa_capital/src/app/auth/login/page.jsx
+++ mansa_capital/src/app/auth/login/page.jsx
@@ -1,8 +1,33 @@
+import { Card,CardHeader,CardContent,CardDescription,CardTitle,CardFooter } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default function Login (){
 
 
     return (
-
-        <>Hello</>
+        <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Account</CardTitle>
+          <CardDescription>
+            Make changes to your account here. Click save when you're done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="username">Username</Label>
+            <Input id="username" defaultValue="@peduarte" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Save changes</Button>
+        </CardFooter>
+      </Card>
+ 
     )
 }
