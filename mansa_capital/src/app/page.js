047fd812d17aca92350a2a9dc8f66905a4c31387
Lokendra B. Jargha <jarghalokendra@gmail.com>
2024-05-17 11:55:23 +0545

    project setup

---
+++ mansa_capital/src/app/page.js
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+<div>
+  You are in homepage
+</div>
+
+  );
+}
