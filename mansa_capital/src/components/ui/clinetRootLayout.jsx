3a315a8bfd18f0a20946c887efd90ca7f36e4e2b
bibash2057 <bibash.thapa7@gmail.com>
2024-05-29 18:12:14 +0545

    footer header sidebar

--- mansa_capital/src/components/ui/clinetRootLayout.jsx
+++ mansa_capital/src/components/ui/clinetRootLayout.jsx
@@ -1,34 +1,24 @@
 "use client";
-import { useState } from "react";
 import Header from "@/components/ui/header";
 import Sidebar from "@/components/ui/sidebar";
 import Footer from "@/components/ui/footer";
 
 const ClientRootLayout = ({ children }) => {
-  //   const [submenuContent, setSubmenuContent] = useState(null);
-
-  //   const handleSubmenuClick = (submenu) => {
-  //     setSubmenuContent(submenu);
-  //   };
-
   return (
-    <>
-      <div className="flex">
-        <div className="w-[20%]">
-          {/* <Sidebar onSubmenuClick={handleSubmenuClick} /> */}
+    <div className="flex flex-col h-screen">
+      <div className="fixed w-full top-0 z-10">
+        <Header />
+      </div>
+      <div className="flex flex-row flex-1 pt-16">
+        <div className="w-[20%] h-full fixed top-16">
           <Sidebar />
         </div>
-        <div className="w-[80%]">
-          <Header />
-          <div className="p-4 flex-1 h-full">
-            {/* {submenuContent === "Results" && <div>Results Content</div>}*/}
-            {children}
-          </div>
-
-          <Footer className="bottom-0 fixed" />
+        <div className="flex flex-col flex-1 ml-[20%] mt-5 overflow-y-auto">
+          <div className="flex-1 mx-4">{children}</div>
+          {/* <Footer className="w-full" /> */}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
